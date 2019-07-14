8c07961fd908d130efb6c35cb8d9f8003d36683e
MorrisMorrison <mwlltrdev@gmail.com>
2019-07-14 18:34:04 +0200

    working heroku build state

--- build.ps1
+++ build.ps1
@@ -4,4 +4,4 @@ ng build
 cd ..
 docker build -t repositoryobserver .
 heroku container:push web -a repositoryobserver
-heroku container:realease web -a repositoryobserver
+heroku container:release web -a repositoryobserver
