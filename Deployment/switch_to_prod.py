150cf9f1f749ee5b36f9c7ae786a9190ae2637eb
MorrisMorrison <mwlltrdev@gmail.com>
2019-10-24 00:24:21 +0200

    add deployment scripts

---
+++ Deployment/switch_to_prod.py
@@ -0,0 +1,33 @@
+import os
+import json
+import fileinput
+
+startup_path = '../RepositoryNotifier/Startup.cs'
+angular_path = '../RepositoryNotifier/ClientApp/angular.json'
+
+with open(angular_path, 'r') as angular_json_file:
+    data = json.load(angular_json_file)
+    data['projects']['RepositoryNotifier']['architect']['build']['options']['outputPath'] = '../repositoryobserver/wwwroot'
+
+os.remove(angular_path)
+with open(angular_path, 'w') as f:
+    json.dump(data, f, indent=4)
+
+
+text_to_search = '// app.UseSpaStaticFiles();'
+replacement_text =  'app.UseSpaStaticFiles();'
+
+with fileinput.FileInput(startup_path, inplace=True, backup='.bak') as file:
+    for line in file:
+        print(line.replace(text_to_search, replacement_text), end='')
+
+
+text_to_search = '// routes.MapSpaFallbackRoute(name: "spa-fallback", defaults: new { controller = "Fallback", action = "Index" });'
+replacement_text =  'routes.MapSpaFallbackRoute(name: "spa-fallback", defaults: new { controller = "Fallback", action = "Index" });'    
+
+with fileinput.FileInput(startup_path, inplace=True, backup='.bak') as file:
+    for line in file:
+        print(line.replace(text_to_search, replacement_text), end='')
+
+
+
--- build.ps1
+++
@@ -1,7 +0,0 @@
-cd RepositoryNotifier
-cd ClientApp
-ng build
-cd ..
-docker build -t repositoryobserver .
-heroku container:push web -a repositoryobserver
-heroku container:release web -a repositoryobserver
